3404e93ae7a2ef33bedf36b8a84ff650e2906a7d
Lev Nagornov <levnagornov@yahoo.com>
2024-01-14 23:01:34 +0100

    initial commit

---
+++ src/test/java/com/love2shop/ecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love2shop.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
